3eaa2fc2edd52efbe51d2f8b780170ba51145e75
javieracevedo <javier.acevedo233@gmail.com>
2024-05-31 12:53:00 -0400

    Use registers from module instead of local ones

--- instructions.py
+++ instructions.py
@@ -1,43 +1,45 @@
 import display
 import registers
-
-#     <-Unused->
-PC = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-
+import memory
+import events
 
 def CLS(surface):
     surface.fill(0)
-
 
 def LD_VX(Vx, kk):
     Vx = int(Vx)
     registers.V[Vx] = kk
 
 def LDF_VX(Vx):
-    Vx_value = V[Vx]
-    print("Vx_value: ", hex(Vx_value))
-    Vx_addr = get_addr(Vx_value)
+    Vx_value = registers.V[Vx]
+    Vx_addr = memory.get_addr(Vx_value)
     if (Vx_addr != ()):
-        register.I = Vx_addr
+        registers.I = Vx_addr
 
-def DRW(Vx, Vy):
-    pass
+def LDX_VK(Vx):
+    key = events.wait_for_keypress();
+    registers.V[Vx] = key
 
-def execute_instructions():
+def DRW(Vx, Vy, surface):
+    display.scaled_draw(int(registers.V[Vx]), int(registers.V[Vy]), surface);
+
+def execute_instructions(instructions, surface):
     for c in range(len(instructions)):
         instruction = instructions[c]
         if (instruction == "00E0"):
-            CLS()
+            CLS(surface)
         elif (instruction[0] == "6"):
             vx = instruction[1]
             kk = instruction[2:]
             LD_VX(vx, kk)
         elif (instruction[0] == "F"):
             vx = instruction[1]
-            LDF_VX(int(vx))
+            if (instruction[2:] == "29"):
+                LDF_VX(int(vx))
+            else:
+                LDX_VK(int(vx))
         elif (instruction[0] == "D"):
             vx = instruction[1]
             vy = instruction[2]
-            DRW(vx, vy)
-
+            DRW(int(vx), int(vy), surface)
 
