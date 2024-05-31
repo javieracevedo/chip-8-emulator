60bb52946bbe8a6288720db3b35688de14ae04cd
javieracevedo <javier.acevedo233@gmail.com>
2024-05-31 12:52:42 -0400

    Remove main loop from display module

--- display.py
+++ display.py
@@ -1,5 +1,7 @@
 import pygame
 import instructions
+import events
+from pygame.locals import *
 
 
 def scaled_draw(x, y, surface):
@@ -9,26 +11,4 @@ def scaled_draw(x, y, surface):
     for pos_x in range(x, x+10):
         for pos_y in range(y, y+10):
             surface.set_at((pos_x, pos_y), pygame.Color(255, 255, 255))
-
-
-def run_display():
-    running = True
-    
-    pygame.init()
-    pygame.display.set_caption("Chip-8 Screen")
-    surface = pygame.display.set_mode((640, 320))
-    
-
-    instructions.CLS(surface)
-
-    while running:
-        scaled_draw(40, 31, surface)
-        scaled_draw(10, 3, surface)
-
-        pygame.display.flip()
-
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-
 
